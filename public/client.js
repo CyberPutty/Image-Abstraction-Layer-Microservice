ecc22a21735bf98321f584e6b6f080bc1a821578
Glitch (super-pruner) <none>
2018-05-15 21:12:19 +0000

    🤒🕰 Checkpoint ./views/index.html:1047756/184 ./public/client.js:1047756/536

--- public/client.js
+++ public/client.js
@@ -1,3 +1,19 @@
 
 
-const name= document.getElementById
+const form= document.getElementById("Image");
+
+
+form.on('submit',function(){ 
+  
+const imageUrl= document.getElementById();
+const title= document.getElementById();
+const source= document.getElmentById();
+  
+$.post("/server.js",{imageUrl: imageUrl, title: title, source: source},function(err,data){
+if (err) console.log(err);
+  if data(
+
+
+});
+
+});
