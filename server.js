10fc5cafadfaedd65672805598a2528cec712863
Glitch (super-pruner) <none>
2018-05-15 19:58:23 +0000

    🏔👘 Checkpoint ./package.json:1047756/2613 ./server.js:1047756/500

--- server.js
+++ server.js
@@ -4,7 +4,19 @@
 // init project
 var express = require('express');
 var app = express();
+const mongoose= require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI);
+
+const  schema= mongoose.Schema;
+
+
+const imageSchema= new schema({
+image: String,
+title: String,
+searched: Date,
+source: String, 
+});
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
