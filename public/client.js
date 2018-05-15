530b53845f181552e471a120d212e21123590f57
Glitch (super-pruner) <none>
2018-05-15 21:43:14 +0000

    🍭⛈ Checkpoint ./public/client.js:1047756/135 ./package.json:1047756/1442 ./views/index.html:1047756/187

--- public/client.js
+++ public/client.js
@@ -1,14 +1,14 @@
 
 
-const $form= $("#image");
+const form= $("#image");
 
-
-$form.on('click',function(){ 
-  
-const $imageUrl= $('#imageUrl');
-const $title= $('#title');
-const $source= $('#source');
- console.log($imageUrl); 
+console.log(form);
+form.click(function(){ 
+  console.log(form);
+const imageUrl= $('#imageUrl').val();
+const title= $('#title').val();
+const source= $('#source').val();
+ console.log(imageUrl); 
 // $.post("/server.js",{imageUrl: $imageUrl, title: $title, source: $source},function(err,data){
 // if (err) console.log(err);
 //   if (data=="done"){
