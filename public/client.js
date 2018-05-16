6e9c95cef2d45e1cc7a2873e7e7c4f92ec601fd2
Glitch (super-pruner) <none>
2018-05-16 02:09:58 +0000

    😽🛣 Checkpoint ./public/client.js:1047756/756

--- public/client.js
+++ public/client.js
@@ -10,17 +10,20 @@ let form= document.getElementById("image");
     let source= document.getElementById('source').value;
     let title= document.getElementById('title').value;
     // set form await form to send to fetch? 
-    if(imageUrl.match()&&source.match()&&){
-    
-    
-    }
-    
+    if(imageUrl.match(/http(s)?:\/\//)&& source&& title){
     let imageData= {imageUrl: imageUrl, title: title, source: source}
   console.log(imageData);
     $.post('https://super-pruner.glitch.me/',imageData,function(data){
     if (data){ console.log("success");}
     });
-      
+    
+    }
+    else{
+    console.log("invalid Url");
+    
+    }
+    
+    
   
   
 
